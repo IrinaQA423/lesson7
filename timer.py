7715183aa4a290ec34b0236d3da8ad8e0fbb2880
IrinaQA423 <irina-koshina@mail.ru>
2025-04-17 19:35:00 +0800

    убрала global

--- timer.py
+++ timer.py
@@ -14,19 +14,19 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=12,
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
-def notify_progress(secs_left, text):
-    bot.create_timer(parse(text), notify_progress)
+def notify_progress(secs_left, text, bot):
+    bot.create_timer(parse(text), lambda: notify_progress(secs_left, text, bot))
+   
 
-
-def say_hello(chat_id, text):
+def say_hello(chat_id, text, bot):
     secs_left = parse(text)
     total_time = secs_left
     message_id = bot.send_message(chat_id, "Запускаю таймер")
     bot.create_countdown(secs_left, notify, chat_id=chat_id,
-                         message_id=message_id, total_time=total_time)
+                         message_id=message_id, total_time=total_time, bot=bot)
 
 
-def notify(secs_left, chat_id, message_id, total_time):
+def notify(secs_left, chat_id, message_id, total_time, bot):
     progress = total_time - secs_left
     progressbar = render_progressbar(total_time, progress)
     bot.update_message(chat_id, message_id,
@@ -37,10 +37,10 @@ def notify(secs_left, chat_id, message_id, total_time):
 
 def main():
     load_dotenv()
-    TG_TOKEN = os.getenv('TELEGRAM_TOKEN')
+    tg_token = os.getenv('TELEGRAM_TOKEN')
 
-    bot = ptbot.Bot(TG_TOKEN)
-    bot.reply_on_message(say_hello)
+    bot = ptbot.Bot(tg_token)
+    bot.reply_on_message(say_hello, bot=bot)
     bot.run_bot()
 
 
